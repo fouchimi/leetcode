915950a54d16c732c39bdf089ca41a8e096d5c92
Fouchimi <ousmanefouchimi@gmail.com>
2018-07-07 18:41:19 -0600

    Delete .idea/workspace.xml file

---
+++ src/Problem458.java
@@ -0,0 +1,14 @@
+public class Problem458 {
+
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+
+        int factor = minutesToDie / minutesToTest;
+
+        return buckets / factor;
+    }
+
+    public static void main(String[] args) {
+        Problem458 problem458 = new Problem458();
+        System.out.println(problem458.poorPigs(1000, 15, 60));
+    }
+}
