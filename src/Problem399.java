0e43e6dbc519a8739c4cef6ce89f45e2157ef619
Fouchimi <ousmanefouchimi@gmail.com>
2018-08-12 07:13:25 -0600

    Added problem399 Evaluate Division

--- src/Problem399.java
+++ src/Problem399.java
@@ -2,58 +2,80 @@ import java.util.*;
 
 public class Problem399 {
 
-    public class Edge {
-        private int ch;
+    class Edge {
+        private String key;
         private double c;
 
-        public Edge(int i, double c) {
-            this.ch = i;
+        Edge(String str, double c) {
+            this.key = str;
             this.c = c;
         }
     }
 
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
 
-        LinkedList<Edge>[] adj = new LinkedList[26];
-        for(int i = 0; i < 26; i++) adj[i] = new LinkedList<>();
-        boolean[] visited = new boolean[26];
+        Map<String, List<Edge>> map = new HashMap<>();
         for(int i = 0; i < equations.length; i++) {
             String first = equations[i][0], second = equations[i][1];
-            int firstIndex = first.charAt(0) - 'a', secondIndex = second.charAt(0) - 'a';
-            LinkedList<Edge> firstList = adj[firstIndex], secondList = adj[secondIndex];
-            firstList.add(new Edge(secondIndex, values[i]));
-            secondList.add(new Edge(firstIndex, 1.0/values[i]));
+            List<Edge> list;
+            if(map.containsKey(first)) {
+                list = map.get(first);
+                list.add(new Edge(second, values[i]));
+                map.put(first, list);
+                if(map.containsKey(second)) {
+                    list = map.get(second);
+                    list.add(new Edge(first, 1.0/values[i]));
+                    map.put(second, list);
+                }else {
+                    list = new ArrayList<>();
+                    list.add(new Edge(first, 1.0/values[i]));
+                    map.put(second, list);
+                }
+            }else {
+                list = new ArrayList<>();
+                list.add(new Edge(second, values[i]));
+                map.put(first, list);
+                if(map.containsKey(second)) {
+                    list = map.get(second);
+                    list.add(new Edge(first, 1.0/values[i]));
+                    map.put(second, list);
+                }else {
+                    list = new ArrayList<>();
+                    list.add(new Edge(first, 1.0/values[i]));
+                    map.put(second, list);
+                }
+            }
         }
 
         double[] res = new double[queries.length];
         for(int i = 0; i < res.length; i++) res[i] = -1.0;
 
+        Set<String> set = new HashSet<>();
         for(int i = 0; i < queries.length; i++){
             String[] query = queries[i];
-            int s = query[0].charAt(0) - 'a';
-            int d = query[1].charAt(0) - 'a';
-            if(!adj[s].isEmpty() && !adj[d].isEmpty()) dfs(adj, s, d, visited, i, res, 1.0);
+            String s = query[0];
+            String d = query[1];
+            if(!map.containsKey(s)) continue;
+            dfs(map, set, s, d, i, res, 1.0);
         }
         return res;
     }
 
-    private void dfs(LinkedList<Edge>[] adj, int s, int d, boolean[] visited, int index, double[] result, double cost) {
-        if(s == d) {
+    private void dfs(Map<String, List<Edge>> map, Set<String> set,  String s, String d,  int index, double[] result, double cost) {
+        if(s.equals(d)) {
             result[index] = cost;
             return;
         }
-        visited[s] = true;
-        for(Edge w : adj[s]){
-            int i = w.ch;
-            if(!visited[i]) dfs(adj, i, d, visited, index, result, cost * w.c);
-        }
-        visited[s] = false;
+        set.add(s);
+        for(Edge w : map.get(s))
+            if(!set.contains(w.key)) dfs(map, set, w.key, d, index, result, cost * w.c);
+        set.remove(s);
     }
 
     public static void main(String[] args){
         Problem399 problem399 = new Problem399();
         String[][] equations = {{"a", "b"}, {"b", "c"}};
-        String[][] queries = { {"a", "c"}, {"b", "a"}, {"a", "e"}, {"a", "a"}, {"x", "x"} };
+        String[][] queries = { {"a", "c"}, {"b", "c"}, {"a", "e"}, {"a", "a"}, {"x", "x"} };
         double[] values = {2.0, 3.0};
         double[] result = problem399.calcEquation(equations, values, queries);
         for(double w : result) System.out.print(w + " ");
