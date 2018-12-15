d3350ff66f3a0abfd1b6c6b07c49655dd6c32432
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2018-12-14 19:30:50 -0700

    Done

--- src/Problem418.java
+++ src/Problem418.java
@@ -1,22 +1,18 @@
 public class Problem418 {
 
     public int wordsTyping(String[] sentence, int rows, int cols) {
-
         int k = 0, counter = 0;
         while (rows > 0) {
             int N = cols;
             while (N > 0) {
                 String next = sentence[k];
-                N = N-next.length() -1;
-                if((k+1 < sentence.length && N >= sentence[k+1].length()) || (k+1 == sentence.length && N > 0)) k++;
-                else if(k+1 < sentence.length && N < sentence[k+1].length()) break;
-                if(k == sentence.length) counter++;
+                if(N == next.length()) N = N-next.length();
+                else if(N > next.length()) N = N-next.length()-1;
+                else break;
+                if(k == sentence.length-1) counter++;
+                k++;
                 k %= sentence.length;
             }
-            if(k >= sentence.length) {
-                k = 0;
-                counter++;
-            }else k++;
             rows--;
         }
         return counter;
@@ -24,7 +20,11 @@ public class Problem418 {
 
     public static void main(String[] args) {
         Problem418 problem418 = new Problem418();
-        String[] sentence = {"a", "bcd", "e"};
-        System.out.println(problem418.wordsTyping(sentence, 3, 6));
+        String[] sentence = {"Hello", "World"};
+        System.out.println(problem418.wordsTyping(sentence, 2, 8));
+        String[] sentence1 = {"a", "bcd", "e"};
+        System.out.println(problem418.wordsTyping(sentence1, 3, 6));
+        String[] sentence2 = {"I", "had", "apple", "pie"};
+        System.out.println(problem418.wordsTyping(sentence2, 4, 5));
     }
 }
