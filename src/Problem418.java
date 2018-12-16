38e0f08a334feaa9b482b292f8ec282f79eb4c3a
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2018-12-16 09:29:22 -0700

    Completed problem418 Sentence fitting screen

--- src/Problem418.java
+++ src/Problem418.java
@@ -1,21 +1,24 @@
 public class Problem418 {
 
     public int wordsTyping(String[] sentence, int rows, int cols) {
-        int k = 0, counter = 0;
+        int k = 0, count = 0;
         while (rows > 0) {
             int N = cols;
             while (N > 0) {
                 String next = sentence[k];
-                if(N == next.length()) N = N-next.length();
-                else if(N > next.length()) N = N-next.length()-1;
-                else break;
-                if(k == sentence.length-1) counter++;
+                int length = next.length();
+                if(N < length) break;
+                else  if(N == length) {
+                    N = N-length;
+                }
+                else N = N-length-1;
+                if(k == sentence.length-1) count++;
                 k++;
                 k %= sentence.length;
             }
             rows--;
         }
-        return counter;
+        return count;
     }
 
     public static void main(String[] args) {
@@ -26,5 +29,13 @@ public class Problem418 {
         System.out.println(problem418.wordsTyping(sentence1, 3, 6));
         String[] sentence2 = {"I", "had", "apple", "pie"};
         System.out.println(problem418.wordsTyping(sentence2, 4, 5));
+        String[] sentence3 = {"a"};
+        System.out.println(problem418.wordsTyping(sentence3, 20000, 20000));
+        String[] sentence4 = {"h"};
+        System.out.println(problem418.wordsTyping(sentence4, 2, 3));
+        String[] sentence5 = {"p"};
+        System.out.println(problem418.wordsTyping(sentence5, 100, 100));
+        String[] sentence6 = {"f", "p", "a"};
+        System.out.println(problem418.wordsTyping(sentence6, 8, 7));
     }
 }
