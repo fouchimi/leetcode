59bf1686fcdccfd4cbc6b90aa288535e349c2813
Fouchimi Ousmane <fouchimi.ousmane@nordstrom.com>
2019-03-12 19:13:49 -0600

    Completed problem 430 Flatten Doubly Linked List

--- src/Problem430.java
+++ src/Problem430.java
@@ -16,25 +16,30 @@ public class Problem430 {
             next = _next;
             child = _child;
         }
+
+        public Node(int _val) {
+            val = _val;
+        }
     }
 
+    private Node prev;
     public Node flatten(Node head) {
         if(head == null) return null;
-        Stack<Node> stack = new Stack<>();
-        Node current = head, newHead = current;
-        while(current.next != null) {
+        Node current = head;
+        prev = head;
+        while (current != null) {
             if(current.child != null) {
-                stack.push(current.next);
-                current.next = current.child;
-                current = current.child;
-            }else current = current.next;
+                Node temp = current.next;
+                current.next = flatten(current.child);
+                current.child = null;
+                current.next.prev = current;
+                prev.next = temp;
+                if(temp != null) temp.prev = prev;
+            }
+            prev = current;
+            current = current.next;
         }
-
-        while (!stack.isEmpty()) {
-            current.next = stack.pop();
-            while (current.next != null) current = current.next;
-        }
-        return newHead;
+        return head;
     }
 
     private void printList(Node head) {
@@ -48,20 +53,40 @@ public class Problem430 {
     public static void main(String[] args) {
         Problem430 problem430 = new Problem430();
 
-        Node firstList = new Node(1, null, null, null);
-        firstList.next = new Node(2, null, null, null);
-        firstList.next.next = new Node(3, null, null, null);
-        firstList.next.next.next = new Node(4, null, null, null);
-        firstList.next.next.next.next = new Node(5, null, null, null);
-        firstList.next.next.next.next.next = new Node(6, null, null, null);
-        firstList.next.next.child = new Node(7, null, null, null);
-        firstList.next.next.child.next = new Node(8, null, null, null);
-        firstList.next.next.child.next.next = new Node(9, null, null, null);
-        firstList.next.next.child.next.next.next = new Node(10, null, null, null);
-        firstList.next.next.child.next.child = new Node(11, null, null, null);
-        firstList.next.next.child.next.child.next = new Node(12, null, null, null);
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+        Node sixth = new Node(6);
+        Node seventh = new Node(7);
+        Node eighth = new Node(8);
+        Node ninth = new Node(9);
+        Node tenth = new Node(10);
+        Node eleventh = new Node(11);
+        Node twelfth = new Node(12);
+        first.next = second;
+        second.prev = first;
+        second.next = third;
+        third.prev = second;
+        third.next = fourth;
+        third.child = seventh;
+        fourth.prev = third;
+        fourth.next = fifth;
+        fifth.prev = fourth;
+        fifth.next = sixth;
+        sixth.prev = fifth;
+        seventh.next = eighth;
+        eighth.prev = seventh;
+        eighth.next = ninth;
+        eighth.child = eleventh;
+        ninth.prev = eighth;
+        ninth.next = tenth;
+        tenth.prev = ninth;
+        eleventh.next = twelfth;
+        twelfth.prev = eleventh;
 
-        Node result = problem430.flatten(firstList);
+        Node result = problem430.flatten(first);
         problem430.printList(result);
 
     }
